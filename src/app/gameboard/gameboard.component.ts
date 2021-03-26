031cd100f37c25e364bda038f14d016ddd1865ea
mvmccart <mvmccart@gmail.com>
2021-03-25 23:59:34 -0400

    format time left

--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -19,7 +19,7 @@ export class GameboardComponent implements OnInit {
   gameLog = [];
   playerScore = 0;
 
-  timeLeft: number = 60;
+  timeLeft: number = 180;
   interval;
   subscribeTimer: any;
 
@@ -51,6 +51,23 @@ export class GameboardComponent implements OnInit {
 
   pauseTimer() {
     clearInterval(this.interval);
+  }
+
+  formatTimeLeft(time) {
+    // The largest round integer less than or equal to the result of time divided being by 60.
+    const minutes = Math.floor(time / 60);
+    
+    // Seconds are the remainder of the time divided by 60 (modulus operator)
+    let seconds = time % 60;
+    
+    // If the value of seconds is less than 10, then display seconds with a leading zero
+    if (seconds < 10) {
+      return `${minutes}:0${seconds}`;
+    } else {
+      return `${minutes}:${seconds}`;
+    }
+  
+    // The output in MM:SS format
   }
 
   correctAnswer(city) {
